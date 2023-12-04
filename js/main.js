6774f6657b48900e03471c00bddb8310c94849bd
Lenin Acosta <rkb.lenin@gmail.com>
2023-12-03 22:19:37 -0500

    opencv added

--- js/main.js
+++ js/main.js
@@ -2,15 +2,25 @@ const app = Vue.createApp({
 	data() {
 		return {
 			img_url: null,
-			kernel_size: 1
+			kernel_size: 1,
+			img_element: null,
 		}
 	},
 	mounted(){
 		this.img_url = "../img/lustre.png";
+		this.img_element = document.getElementById("img_original");
 	},
 	methods: {
 		loadimg(e){
 			this.img_url = URL.createObjectURL(e.target.files[0]);
+			this.img_element = document.getElementById("img_original");
 		},
+		do_something(){
+			let img = cv.imread(this.img_element);
+			this.media();
+		},
+		media(){
+
+		}
 	},
 });
